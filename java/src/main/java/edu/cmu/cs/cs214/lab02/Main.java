cbc4d8690826f66f268c793036b90c03cfb146de
bolorierie <lynklynk56@gmail.com>
2025-02-14 09:58:43 +0800

    labbuteelt3

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,55 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Scanner scanner = new Scanner(System.in);
+        Shape shape = null;
 
-        Renderer renderer = new Renderer(rectangle);
+        try {
+            System.out.println("Select a shape to render:");
+            System.out.println("1. Rectangle");
+            System.out.println("2. Circle");
+            System.out.println("3. Square");
 
-        renderer.draw();
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter width: ");
+                    double width = scanner.nextDouble();
+                    System.out.print("Enter height: ");
+                    double height = scanner.nextDouble();
+                    shape = new Rectangle(width, height);
+                    break;
+                case 2:
+                    System.out.print("Enter radius: ");
+                    double radius = scanner.nextDouble();
+                    shape = new Circle(radius);
+                    break;
+                case 3:
+                    System.out.print("Enter side length: ");
+                    double sideLen = scanner.nextDouble();
+                    shape = new Square(sideLen);
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    System.exit(1);
+            }
+
+            Renderer renderer = new Renderer(shape);
+            renderer.draw();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter numeric values.");
+        } finally {
+            scanner.close();
+        }
     }
 }
